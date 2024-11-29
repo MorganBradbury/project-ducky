aec499aa6979f3fbf3a89d3086908d66048d8c37
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2024-11-29 13:17:41 +0000

    Change formatting

--- src/services/discordHandler.ts
+++ src/services/discordHandler.ts
@@ -89,10 +89,10 @@ const getEloDifference = async (previousElo: number, gamePlayerId: string) => {
 
   if (faceitPlayer.faceit_elo > previousElo) {
     const eloChange = faceitPlayer.faceit_elo - previousElo;
-    return `${`(${previousElo} > ${faceitPlayer.faceit_elo}) **+${eloChange}**`}`;
+    return `${`(${faceitPlayer.faceit_elo}) **\+${eloChange}\**`}`;
   } else {
     const eloChange = previousElo - faceitPlayer?.faceit_elo;
-    return `${`(${previousElo} > ${faceitPlayer.faceit_elo}) **-${eloChange}**`}`;
+    return `${`(${faceitPlayer.faceit_elo}) **\+${eloChange}\**`}`;
   }
 };
 
@@ -128,7 +128,7 @@ export const sendMatchFinishNotification = async (
         {
           name: "Match Result",
           value: `${matchDetails.results?.finalScore} (${
-            matchDetails.results?.win ? "🟢 WIN" : "🔴 LOSS"
+            matchDetails.results?.win ? "WIN" : "LOSS"
           })`,
         },
         {
