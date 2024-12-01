4f329e3f3af1fa1339848fb52a1f1483eac9e610
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2024-12-01 23:45:22 +0000

    COMMENTED OUT DELETE FOR NOW

--- src/services/matchesService.ts
+++ src/services/matchesService.ts
@@ -195,7 +195,7 @@ export const cancelMatch = async (matchId: string) => {
       await new Promise((resolve) => setTimeout(resolve, 1000)); // 1-second delay
 
       // Delete the old voice channel
-      await deleteVoiceChannel(voiceChannelId);
+      //await deleteVoiceChannel(voiceChannelId);
 
       console.log(
         `Moved users from voiceChannelId: ${voiceChannelId} to newChannelId: ${newChannelId}`
