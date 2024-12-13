dee0c6619599a77ae7edbc097848b3277cb24e49
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2024-12-13 18:48:49 +0000

    added logging

--- src/api/services/minecraftService.ts
+++ src/api/services/minecraftService.ts
@@ -4,13 +4,14 @@ import { config } from "../../config";
 /**
  * Fetch the current player count from the Minecraft server.
  */
-export const minecraftActivePlayers = async (): Promise<string[] | null> => {
+export const minecraftActivePlayers = async (): Promise<[] | null> => {
   const HOST = config.MINECRAFT_SERVER_IP; // Replace with your server's IP
   const PORT = config.MINECRAFT_SERVER_PORT; // Replace if your query port differs
 
   try {
     // Fetch server information
     const serverInfo: any = await queryFull(HOST, Number(PORT));
+    console.log(serverInfo);
     return serverInfo?.players.online || null; // Return the player count or null if it's unavailable
   } catch (error) {
     console.error("Error fetching player count:", error);
