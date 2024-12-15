568c65f3dabb99200aa44b92cff828502b63b9ae
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2024-12-15 18:39:17 +0000

    Added missing intent

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -7,6 +7,7 @@ export const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildVoiceStates, // Add this intent to enable voiceStateUpdate
   ],
   partials: [Partials.Message, Partials.Channel],
 });
