dab05ee48715f6874911f2c63167967d80e36ad9
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2024-12-30 15:53:12 +0000

    Added updating into unicode

--- src/utils/nicknameUtils.ts
+++ src/utils/nicknameUtils.ts
@@ -1,8 +1,29 @@
 import { GuildMember } from "discord.js";
 import { Player } from "../types/Faceit/Player";
+import { eloNumbers } from "../constants";
+
+// function removeExistingTag(nickname: string): string {
+//   return nickname.replace(/\s?\[.*?\]/, "").trim();
+// }
 
 function removeExistingTag(nickname: string): string {
   return nickname.replace(/\s?\[.*?\]/, "").trim();
+}
+
+function removeUnicodeChars(input: string): string {
+  // Regular expression to match Unicode superscript digits (U+2070 to U+209F) and brackets (U+207D, U+207E)
+  const pattern = /[\u2070-\u209F\u207D\u207E]/g;
+
+  // Replace any character matching the pattern with an empty string
+  return input.replace(pattern, "");
+}
+
+function toEloUnicode(input: string): string {
+  // Map each character in the string to its eloNumbers equivalent, if it exists
+  return input
+    .split("")
+    .map((char) => eloNumbers[char] || char)
+    .join("");
 }
 
 export function isNickname(identifier: string | number): identifier is string {
@@ -16,10 +37,10 @@ export async function updateNickname(
   if (!player) return;
 
   const currentName = member.nickname || member.user.username;
-  const cleanName = removeExistingTag(currentName);
+  const cleanName = removeUnicodeChars(currentName);
 
   // Calculate the length of the clean name and the ELO to check if the total exceeds 32 characters
-  const eloTag = `[${player.faceitElo}]`;
+  const eloTag = toEloUnicode(`[${player.faceitElo}]`);
   const potentialNickname = `${cleanName} ${eloTag}`;
 
   // If the nickname exceeds 32 characters, use the Discord username instead of the nickname
