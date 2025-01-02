bb4fbfc27dbfbc3e470fa3dbf522145ec01af50e
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2025-01-02 22:12:33 +0000

    Temporarily disable prematch analysis

--- src/api/controllers/MatchesController.ts
+++ src/api/controllers/MatchesController.ts
@@ -39,7 +39,7 @@ export const handleMatchesHook = async (
 
     if (eventId === AcceptedEventTypes.match_created) {
       console.log("Request received for match being configured: ", req.body);
-      await sendPrematchAnalysis(matchId);
+      //await sendPrematchAnalysis(matchId);
     }
 
     // Match has just started
