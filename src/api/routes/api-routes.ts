e698fccf54563d7779077215d074144c29461df2
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2025-01-20 18:17:41 +0000

    refactor: Remove Minecraft-related controller, service, and command files

--- src/api/routes/api-routes.ts
+++ src/api/routes/api-routes.ts
@@ -3,11 +3,8 @@ import {
   handleMatchesHook,
   updateLiveScores,
 } from "../controllers/matches-controller";
-import { getPlayerCount } from "../controllers/minecraft-controller";
 
 export const apiRoutes = express.Router();
 
-// Webhook callback endpoint
 apiRoutes.post("/webhook", handleMatchesHook);
 apiRoutes.post("/updatelivescores", updateLiveScores);
-apiRoutes.post("/minecraft/playercount", getPlayerCount);
--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,7 +1,6 @@
 import { deleteUserCommand } from "./Faceit/delete-tracking";
 import { helpCommand } from "./help";
 import { leaderboardCommand } from "./Faceit/leaderboard";
-import { getActivePlayers } from "./Minecraft/get-active-players";
 import { clearMessagesCommand } from "./Discord/clear-messages";
 import { updateNicknameCommand } from "./Discord/set-nickname";
 import { listUserIdsCommand } from "./Faceit/list-tracked-ids";
@@ -11,7 +10,6 @@ export const commands = [
   deleteUserCommand,
   helpCommand,
   leaderboardCommand,
-  getActivePlayers,
   clearMessagesCommand,
   updateNicknameCommand,
   listUserIdsCommand,
