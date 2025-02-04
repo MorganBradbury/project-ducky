5e9557972c041d3fb7ffae1281468e882b981f58
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2025-02-04 20:41:03 +0000

    fix: Remove unnecessary timestamp from match end notification embed

--- src/api/services/embedService.ts
+++ src/api/services/embedService.ts
@@ -110,8 +110,7 @@ export async function matchEndNotification(match: Match) {
           value: `[🔗 Link](${LINKS.MATCHROOM}/${match?.matchId})`,
         }
       )
-      .setFooter({ text: `${match.matchId}` })
-      .setTimestamp();
+      .setFooter({ text: `${match.matchId}` });
 
     await sendEmbedMessage(embed, config.CHANNEL_MATCH_RESULTS, match.matchId);
   } catch (error) {
