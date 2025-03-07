18ac7476426e2b50ed98c86d5b84ae4ade37a8c4
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2025-03-07 10:09:12 +0000

    feat: add join route to redirect to Steam connect link using IP and port

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -15,3 +15,10 @@ apiRoutes.post("/archivethreads", archiveMatches);
 apiRoutes.post("/updateleaderboard", updateLeaderboard);
 apiRoutes.post("/createverifieduser", createUser);
 apiRoutes.delete("/deleteuser", deleteSingleUser);
+
+apiRoutes.get("/join/:ip/:port", (req, res) => {
+  const { ip, port } = req.params;
+  const steamConnectLink = `steam://connect/${ip}:${port}`;
+
+  res.redirect(steamConnectLink);
+});
