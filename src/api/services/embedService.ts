037a007f1b7d9aa4ece62c7e426d91fda9d5b663
Morgan Bradbury <morgan.bradbury@hotmail.co.uk>
2025-03-17 20:33:56 +0000

    fix: update scoreboard field name for clarity in match end notification

--- src/api/services/embedService.ts
+++ src/api/services/embedService.ts
@@ -125,7 +125,7 @@ export async function matchEndNotification(match: Match) {
         })`
       )
       .addFields({
-        name: "Scoreboard (𝖪/𝖣/𝖠)",
+        name: "Scoreboard (K/D/A)",
         value: `${playerStatsTable.join("\n")}`,
       })
       .setURL(`${LINKS.MATCHROOM}/${match?.matchId}`)
